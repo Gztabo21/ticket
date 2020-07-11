1f37168487b9d8f579670f567923e3ca07df29aa
Gztabo21 <Gustavocacharuco@gmail.com>
2020-07-11 12:32:11 -0400

    cambios

--- views-ticket/src/components/ticket.jsx
+++ views-ticket/src/components/ticket.jsx
@@ -1,6 +1,6 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useRef} from 'react';
 import TicketService from '../core/services/ticket-services';
-/* import TableDataSource from './table'; */
+ import TableDataSource from './table'; 
 import { Table } from 'react-bootstrap';
 import TicketForm from'./ticket-form';
 import ConfirmDelete from './confirm-delete';
@@ -12,14 +12,9 @@ import './ticket.css';
 
 export default function Ticket(props){
     const [ticket,setTicket] = useState([])
-    console.log(props.idUser)
+    const wrapper = useRef(null);
     useEffect(()=>{
-     /*  props.iduser ?
-      TicketService.getTicketAssigned(props.iduser).then(res =>{
-          console.log(res.data);
-        setTicket(res.data)
-      })
-      : */  
+   
       TicketService.getAll().then(res =>{
         setTicket(res.data)
       })
@@ -28,18 +23,18 @@ export default function Ticket(props){
     
 return(
     <>
-    <Container>
-        <Row>
+    <Container ref={wrapper} >
+        <Row ref={wrapper} >
             <Col md={{ offset:4, span:3 }}><h1>Tickets</h1></Col>
         </Row>
-        <Row>
+        <Row ref={wrapper}  >
             <Col className='header-btns'><TicketForm name="NEW"/></Col>
         </Row>
     </Container>
     <Container>
         
-        <Row>
-            <Col>
+        <Row ref={wrapper}  >
+            <Col ref={wrapper} >
             {/* <TableDataSource data={ticket} /> */}
             <Table striped bordered hover variant="dark">
             <thead>
